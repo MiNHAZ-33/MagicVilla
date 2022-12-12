e29a4867bf04df7f1ccb891bc39ee8449eba58f6
MiNHAZ-33 <miminhaz33@gmail.com>
2022-12-13 00:55:17 +0600

    Post method is completed

--- MagicVilla_Villa_API/Controllers/VillaAPIController.cs
+++ MagicVilla_Villa_API/Controllers/VillaAPIController.cs
@@ -1,4 +1,6 @@
-﻿using MagicVilla_Villa_API.Models;
+﻿using MagicVilla_Villa_API.Data;
+using MagicVilla_Villa_API.Models;
+using MagicVilla_Villa_API.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_Villa_API.Controllers
@@ -8,13 +10,50 @@ namespace MagicVilla_Villa_API.Controllers
     public class VillaAPIController : Controller
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<Villa>
+            return Ok(VillaStrore.villaList) ;
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
             {
-                new Villa {Id=1, Name="Heaven"},
-                new Villa {Id=2, Name="Second"}
-            };
+                return BadRequest();
+            }
+
+            var villa = VillaStrore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villa){
+            if(villa == null)
+            {
+                return BadRequest(villa);
+            }
+            if(villa.Id < 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villa.Id = VillaStrore.villaList.OrderByDescending(u=>u.Id).FirstOrDefault().Id + 1;
+
+            VillaStrore.villaList.Add(villa);
+
+            return Ok(villa);
         }
     }
 }
---
+++ MagicVilla_Villa_API/Models/DTO/VillaDTO.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace MagicVilla_Villa_API.Models.DTO
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
