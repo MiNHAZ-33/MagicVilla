df4a7c35c7fb5f96fc57e65305b90bb1c077acf3
MiNHAZ-33 <miminhaz33@gmail.com>
2022-12-12 23:33:41 +0600

    Add project files.

---
+++ MagicVilla_Villa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_Villa_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Villa_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id=1, Name="Heaven"},
+                new Villa {Id=2, Name="Second"}
+            };
+        }
+    }
+}
