8d7fd10fa4ab97e79a442f794c19470a9e869c33
MiNHAZ-33 <miminhaz33@gmail.com>
2023-03-11 11:03:56 +0600

    added base service

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseServices
+    {
+        public APIResponse responseModel { get; set; }
+        
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+
+        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MagicAPI");
+                HttpRequestMessage messsage = new HttpRequestMessage();
+                messsage.Headers.Add("Content-Type", "application/json");
+                messsage.RequestUri = new Uri(apiRequest.Url);
+
+                if(apiRequest.Data!= null)
+                {
+                    messsage.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+
+            }
+        }
+    }
+}
