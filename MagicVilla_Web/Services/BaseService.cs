9522d1b16512917af61ac1ad5aad6c72511c1e72
MiNHAZ-33 <miminhaz33@gmail.com>
2023-03-11 11:13:11 +0600

    fixed error in base service

--- MagicVilla_Web/Services/BaseService.cs
+++ MagicVilla_Web/Services/BaseService.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_Web.Models;
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
 using MagicVilla_Web.Services.IServices;
 using Newtonsoft.Json;
 using System.Text;
@@ -17,7 +18,7 @@ namespace MagicVilla_Web.Services
             this.httpClient = httpClient;
         }
 
-        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
         {
             try
             {
@@ -26,13 +27,46 @@ namespace MagicVilla_Web.Services
                 messsage.Headers.Add("Content-Type", "application/json");
                 messsage.RequestUri = new Uri(apiRequest.Url);
 
-                if(apiRequest.Data!= null)
+                if (apiRequest.Data != null)
                 {
                     messsage.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
                         Encoding.UTF8, "application/json");
                 }
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        messsage.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        messsage.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        messsage.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        messsage.Method = HttpMethod.Get;
+                        break;
+                }
 
+                HttpResponseMessage apiResponse = null;
+                apiResponse = await client.SendAsync(messsage);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+
+                return APIResponse;
+            } catch (Exception ex)
+            {
+                var DTO = new APIResponse
+                {
+                    ErrorMessage = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false,
+                };
+                var res = JsonConvert.SerializeObject(DTO);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
             }
+            
         }
     }
 }
